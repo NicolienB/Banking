629bca765132cb10d367f2f93c0d9b8a51637002
Nicolien Boekhoudt <nicolien.boekhoudt@hotmail.nl>
2021-03-12 23:41:08 +0100

    Added Tests, date validation fails

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Transaction < ApplicationRecord
-  Currencylist = ["Euro", "Dollar $"]
+  Currencylist = ["Euro €", "Dollar $"]
   CreditOrDebitList = ["Credit","Debit"]
   validates :bank_account, :contra_account, format: {with: /[a-zA-Z]{2}[\s]*[0-9]{2}[\s]*[a-zA-Z]{4}[\s]*[0-9]{14}[\s]*/, message: "Please use the following format for the IBAN: GB92 AVVZ 46534230174644 "}
   validates :contra_account_owner, format: {with:/[a-zA-Z \s*]/, message: "Only letters and whitespaces are allowed"}
--- test/models/transaction_test.rb
+++ test/models/transaction_test.rb
@@ -4,4 +4,54 @@ class TransactionTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  #following tests check if required fields work as expected, the names are self explanatory
+  test "is valid with all valid attributes" do
+    transaction = Transaction.new(description:"This is some free money",bank_account: "GB92 AVVZ 46534230174644", contra_account: "GB92 AVVZ 46534230174644", date:Date.today,contra_account_owner:"Paul",amount: 10, currency: "Euro €",credit_or_debit: "Credit" )
+    assert transaction.save
+  end
+  test "is valid without description,but valid attributes" do
+    transaction = Transaction.new(bank_account: "GB92 AVVZ 46534230174644", contra_account: "GB92 AVVZ 46534230174644", date:Date.today,contra_account_owner:"Paul",amount: 10, currency: "Euro €",credit_or_debit: "Credit" )
+    assert transaction.save
+  end
+  test "is invalid without bank_account" do
+    transaction = Transaction.new(description:"This is some free money", contra_account: "GB92 AVVZ 46534230174644", date:Date.today,contra_account_owner:"Paul",amount: 10, currency: "Euro €",credit_or_debit: "Credit" )
+    assert_not transaction.save
+  end
+  test "is invalid without contra_account" do
+    transaction = Transaction.new(description:"This is some free money",bank_account: "GB92 AVVZ 46534230174644", date:Date.today,contra_account_owner:"Paul",amount: 10, currency: "Euro €",credit_or_debit: "Credit" )
+    assert_not transaction.save
+  end
+  test "is invalid without Date" do
+    transaction = Transaction.new(description:"This is some free money",bank_account: "GB92 AVVZ 46534230174644", contra_account: "GB92 AVVZ 46534230174644", contra_account_owner:"Paul",amount: 10, currency: "Euro €",credit_or_debit: "Credit" )
+    assert_not transaction.save
+  end
+  test "is invalid without contra account owner" do
+    transaction = Transaction.new(description:"This is some free money",bank_account: "GB92 AVVZ 46534230174644", contra_account: "GB92 AVVZ 46534230174644", date:Date.today,amount: 10, currency: "Euro €",credit_or_debit: "Credit" )
+    assert_not transaction.save
+  end
+  test "is invalid without amount" do
+    transaction = Transaction.new(description:"This is some free money",bank_account: "GB92 AVVZ 46534230174644", contra_account: "GB92 AVVZ 46534230174644", date:Date.today,contra_account_owner:"Paul", currency: "Euro €",credit_or_debit: "Credit" )
+    assert_not transaction.save
+  end
+  test "is invalid without currency" do
+    transaction = Transaction.new(description:"This is some free money",bank_account: "GB92 AVVZ 46534230174644", contra_account: "GB92 AVVZ 46534230174644", date:Date.today,contra_account_owner:"Paul",amount: 10,credit_or_debit: "Credit" )
+    assert_not transaction.save
+  end
+  test "is invalid without Credit or Debit" do
+    transaction = Transaction.new(description:"This is some free money",bank_account: "GB92 AVVZ 46534230174644", contra_account: "GB92 AVVZ 46534230174644", date:Date.today,contra_account_owner:"Paul",amount: 10, currency: "Euro €" )
+    assert_not transaction.save
+  end
+#  The following tests test if only valid arguments are accepted
+  test "is invalid with wrong bank account" do
+    transaction = Transaction.new(description:"This is some free money",bank_account: "GB9 AVVZ 46534230174644", contra_account: "GB92 AVVZ 46534230174644", date:Date.today,contra_account_owner:"Paul",amount: 10, currency: "Euro €",credit_or_debit: "Credit" )
+    assert_not transaction.save
+  end
+  test "is invalid with wrong contra account" do
+    transaction = Transaction.new(description:"This is some free money",bank_account: "GB92 AVVZ 46534230174644", contra_account: "GB9 AVVZ 46534230174644", date:Date.today,contra_account_owner:"Paul",amount: 10, currency: "Euro €",credit_or_debit: "Credit" )
+    assert_not transaction.save
+  end
+  test "is invalid with date in past" do
+    transaction = Transaction.new(description:"This is some free money",bank_account: "GB92 AVVZ 46534230174644", contra_account: "GB92 AVVZ 46534230174644", date: 2021-03-10,contra_account_owner:"Paul",amount: 10, currency: "Euro €",credit_or_debit: "Credit" )
+    assert_not transaction.save
+  end
 end
