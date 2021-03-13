c7663e3848b87ebe5dc50f4a7375fbd096f5c5cb
Nicolien Boekhoudt <nicolien.boekhoudt@hotmail.nl>
2021-03-13 22:09:38 +0100

    Added comments to validations

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,21 +1,21 @@
 # frozen_string_literal: true
 
 class Transaction < ApplicationRecord
-  # extend AutoStripAttributes
-  # include StripWhitespace
-  # strip_whitespace_from_attributes : bank_account
-  # before_validation auto_strip_attributes :bank_account, :contra_account
-  # before_validation.gsub(/\s+/, "")
+  #Delete white spaces for both bank accounts
   before_validation :delete_whitespace
   Currencylist = ["€", "$"]
   CreditOrDebitList = ["Credit","Debit"]
+  #Ensures only valid bank accounts are accepted
   validates :bank_account, :contra_account,  format: {with: /\A[a-zA-Z]{2}[\s]*[0-9]{2}[\s]*[a-zA-Z]{4}[\s]*[0-9]{14}[\s]*\z/, message: "Please use the following format for the IBAN: GB92 AVVZ 46534230174644 "}
   validates :bank_account, :contra_account, length:{maximum: 22}
-  validates :contra_account_owner, format: {with:/[a-zA-Z \s*]/, message: "Only letters and whitespaces are allowed"}
-  # validates :date >= Date.now
+  #Ensures no characters other than whitespaces and letters can be used.
+  validates :contra_account_owner, format: {with:/\A[a-zA-Z \s*]\z/, message: "Only letters and whitespaces are allowed"}
+  #all fields are mandatory except for description  field
   validates :date, :bank_account, :contra_account, :contra_account_owner, :amount, :currency, :credit_or_debit,
             presence: true
+  #you cannot make a transaction in the past
   validate :date, :DateValidation
+  #Ensures the input amount is bigger than xero and the maximum amount of decimals is 2
   validates :amount, :numericality => { greater_than: 0, message: "The amount you want to transfer needs to be bigger than 0 represented in numbers, please use . as decimal point" }
   validates :amount, presence: true, format: { with: /\A\d+(?:\.\d{0,2})?\z/, message:"the amount cannot have more than two decimals"}
   # before_validation :bank_account,   :strip_whitespace
